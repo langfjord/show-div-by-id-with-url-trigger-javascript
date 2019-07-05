bebd936e3e499011366ba8126b6a69a5f809c403
langfjord <16120146+langfjord@users.noreply.github.com>
2019-07-05 15:20:13 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,19 @@
+<script>
+  var url = window.location.href;
+
+	  var divtoshow = document.getElementById('show1');
+	  if( url.search( 'show1' ) > 0 ) {
+		  divtoshow.style.display = "block";
+	  }
+    
+    var divtoshow = document.getElementById('show2');
+	  if( url.search( 'show2' ) > 0 ) {
+		  divtoshow.style.display = "block";
+	  }
+    
+    var divtoshow = document.getElementById('show3');
+	  if( url.search( 'show3' ) > 0 ) {
+		  divtoshow.style.display = "block";
+	  }
+        
+</script>
